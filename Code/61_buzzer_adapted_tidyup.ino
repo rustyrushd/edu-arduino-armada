e77774812f12028263323777cacf98ec1375e1f1
Rushdius Gluteus Maximus <204103529+rustyrushd@users.noreply.github.com>
2025-05-05 21:00:52 +0100

    Upload buzzer code

---
+++ Code/61_buzzer_adapted_tidyup.ino
@@ -0,0 +1,33 @@
+// adapted buzzer code to play for 4 seconds by lengthening pause between notes
+// and code is cleaned up into a function ready for reuse
+
+const int BZR = 8;
+
+#define NOTE_C4  262
+#define NOTE_G3  196
+#define NOTE_A3  220
+#define NOTE_B3  247
+#define NOTE_C4  262
+
+void buzzer() {
+  int melody[] = {
+    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  };
+  int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4};
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BZR, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 2.0;
+    delay(pauseBetweenNotes);
+    noTone(BZR);
+  }
+}
+
+void setup() {
+  pinMode(BZR, OUTPUT);
+  buzzer();
+}
+
+void loop() {
+  // do nothing
+}
